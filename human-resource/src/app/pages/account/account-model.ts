600bb6c349c0a3a8e5eebeda2d22ee77b63c8857
Huỳnh Trọng Trí <63443694+HuynhTrongTri@users.noreply.github.com>
2021-07-16 18:34:37 +0700

    Delete human-resource directory

--- human-resource/src/app/pages/account/account-model.ts
+++
@@ -1,9 +0,0 @@
-export interface EmployeeAccount {
-    id?: string;
-    fullname?: string;
-    username?: string;
-    totalDayWorking?: number;
-    isDeleted?: boolean;
-    gender?: string;
-    email?: string;
-}
